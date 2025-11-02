c7a4935d84409893625b9e09f403e26aa5664e36
BrayanTorres <brayantorresacuna2023@gmail.com>
2025-11-02 10:00:11 -0500

    Cambios para agregar autorización a los endpoints

--- src/main/java/com/pe/vethope/vethope_backend/controller/ProductoController.java
+++ src/main/java/com/pe/vethope/vethope_backend/controller/ProductoController.java
@@ -9,6 +9,7 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -19,6 +20,7 @@ import java.util.List;
 @RequestMapping("/api/v1/productos")
 @RequiredArgsConstructor
 @Tag(name = "Productos", description = "Operaciones relacionadas con la gestión de productos")
+@PreAuthorize("hasAnyRole('ADMIN', 'RECEPCIONISTA')")
 public class ProductoController {
 
     private final ProductoService productoService;
