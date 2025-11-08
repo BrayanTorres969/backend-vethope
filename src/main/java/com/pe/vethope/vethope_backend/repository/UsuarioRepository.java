b9b81c932a881ed3b2fda7497c37d5d779f1d5c2
BrayanTorres <brayantorresacuna2023@gmail.com>
2025-11-08 12:56:47 -0500

    Agregar endpoints para CRUD de usuarios

--- src/main/java/com/pe/vethope/vethope_backend/repository/UsuarioRepository.java
+++ src/main/java/com/pe/vethope/vethope_backend/repository/UsuarioRepository.java
@@ -4,12 +4,15 @@ import com.pe.vethope.vethope_backend.entity.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
     Optional<Usuario> findByUsername(String username);
     Optional<Usuario> findByCorreo(String correo);
+    List<Usuario> findAllByActivoTrue();
+    List<Usuario> findAllByActivoFalse();
     Boolean existsByUsername(String username);
     Boolean existsByCorreo(String correo);
     Optional<Usuario> findByUsernameAndActivoTrue(String username);
