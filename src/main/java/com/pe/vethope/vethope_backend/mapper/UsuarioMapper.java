4943f41d937a5c9d90b0a006fe56d57426e222b4
BrayanTorres <brayantorresacuna2023@gmail.com>
2025-11-09 15:35:09 -0500

    Agregar anotacion Mapper faltante

--- src/main/java/com/pe/vethope/vethope_backend/mapper/UsuarioMapper.java
+++ src/main/java/com/pe/vethope/vethope_backend/mapper/UsuarioMapper.java
@@ -2,9 +2,12 @@ package com.pe.vethope.vethope_backend.mapper;
 
 import com.pe.vethope.vethope_backend.dto.UserDTO;
 import com.pe.vethope.vethope_backend.entity.Usuario;
+import org.mapstruct.Mapper;
 
 import java.util.List;
 
+
+@Mapper(componentModel = "spring")
 public interface UsuarioMapper {
     UserDTO toDTO(Usuario usuario);
     Usuario toEntity(UserDTO usuarioDto);
