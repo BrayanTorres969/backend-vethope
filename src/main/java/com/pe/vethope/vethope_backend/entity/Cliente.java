f4d76dd329fe76b92e01bdf72dfa85025527d807
BrayanTorres <brayantorresacuna2023@gmail.com>
2025-12-04 21:26:19 -0500

    Agregar id a clientes y prodcutos en el response de listado

--- src/main/java/com/pe/vethope/vethope_backend/entity/Cliente.java
+++ src/main/java/com/pe/vethope/vethope_backend/entity/Cliente.java
@@ -17,6 +17,7 @@ public class Cliente {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_cliente")
     private Long id_cliente;
 
     private String dni;
--- src/main/java/com/pe/vethope/vethope_backend/mapper/ProductoMapper.java
+++ src/main/java/com/pe/vethope/vethope_backend/mapper/ProductoMapper.java
@@ -3,13 +3,15 @@ package com.pe.vethope.vethope_backend.mapper;
 import com.pe.vethope.vethope_backend.dto.ProductoDTO;
 import com.pe.vethope.vethope_backend.entity.Producto;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 import java.util.List;
 
 @Mapper(componentModel = "spring")
 public interface ProductoMapper {
-
+    @Mapping(source = "id_producto", target = "id_producto")
     ProductoDTO toDTO(Producto producto);
+    @Mapping(source = "id_producto", target = "id_producto")
     Producto toEntity(ProductoDTO productoDTO);
     List<ProductoDTO> toDTOList(List<Producto> productos);
 }
